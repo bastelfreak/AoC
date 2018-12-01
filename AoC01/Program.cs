77dc55479df5c0b87b2a1cfc6c03e51ac11bbb1c
bastelfreak <tim@bastelfreak.de>
2018-12-01 10:59:21 +0100

    Add project files.

---
+++ AoC01/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace AoC01
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
